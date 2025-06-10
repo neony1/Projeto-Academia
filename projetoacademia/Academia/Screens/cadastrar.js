71585d840603e43d47074957f5bb800d6f1d52c3
Pedro Henrique F. <pedrohenriqueferreira9898@gmail.com>
2025-06-10 17:27:32 -0300

    coloquei o espaço
    só issoo

--- projetoacademia/Academia/Screens/cadastrar.js
+++ projetoacademia/Academia/Screens/cadastrar.js
@@ -35,56 +35,61 @@ export default function Cadastrar({navigation}) {
                 placeholder= "Email" // email
                 value={email} // valor do input
                 onChangeText={setEmail} // função que pega o valor do input
-            />
+            /><br></br>
             <TextInput
                 style={styles.txtinput}
                 placeholder="Senha" // senha
                 value={senha} // valor do input
                 onChangeText={setSenha} // função que pega o valor do input
                 secureTextEntry={true} // para esconder a senha (ficam em asterisco)
+            /><br></br>
+            <Button
+                title="Login"
+                color={'orange'}
+                onPress={() => navigation.navigate('Telalogin')}
             />
             <TextInput
                 style={styles.txtinput}
                 placeholder="Nome" // senha
                 value={nome} // valor do input
                 onChangeText={setNome} // função que pega o valor do input
-            />
+            /><br></br>
             <TextInput
                 style={styles.txtinput}
                 placeholder="Idade" // senha
                 value={idade} // valor do input
                 onChangeText={setIdade} // função que pega o valor do input
-            />
+            /><br></br>
             <TextInput
                 style={styles.txtinput}
                 placeholder="Altura" // senha
                 value={altura} // valor do input
                 onChangeText={setAltura} // função que pega o valor do input
-            />
+            /><br></br>
             <TextInput
                 style={styles.txtinput}
                 placeholder="Peso atual" // senha
                 value={pesoAtual} // valor do input
                 onChangeText={setPesoAtual} // função que pega o valor do input
-            />
+            /><br></br>
             <TextInput
                 style={styles.txtinput}
                 placeholder="IMC" // senha
                 value={imc} // valor do input
                 onChangeText={setImc} // função que pega o valor do input
-            />
+            /><br></br>
             <TextInput
                 style={styles.txtinput}
                 placeholder="Nivel" // senha
                 value={nivel} // valor do input
                 onChangeText={setNivel} // função que pega o valor do input
-            />
+            /><br></br>
             <TextInput
                 style={styles.txtinput}
                 placeholder="Objetivo" // senha
                 value={objetivo} // valor do input
                 onChangeText={setObjetivo} // função que pega o valor do input
-            />
+            /><br></br>
             <Button
                 title="Login"
                 color={'orange'}
